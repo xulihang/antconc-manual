56517e12864215bcefce5d18198ce3a911e4cfda
xulihang <xulihanghai@163.com>
2018-06-02 19:58:16 +0800

    use ctex

--- source/conf.py
+++ source/conf.py
@@ -114,24 +114,11 @@ latex_elements = {
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
+    'fontpkg':'',
+    'fncychap':'',
     # Additional stuff for the LaTeX preamble.
     #
-    'preamble': '''
-\hypersetup{unicode=true}
-\usepackage{CJKutf8}
-\DeclareUnicodeCharacter{00A0}{\nobreakspace}
-\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
-\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
-\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
-\DeclareUnicodeCharacter{2713}{x}
-\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
-\DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
-\DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
-\DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
-\DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-\begin{CJK}{UTF8}{gbsn}
-\AtEndDocument{\end{CJK}}''',
+    'preamble': '''\usepackage[UTF8, scheme = plain]{ctex}''',
 
     # Latex figure (float) alignment
     #
